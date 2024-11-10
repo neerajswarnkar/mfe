9a99257c6987dab121358adcdb54e57f0c81f78e
neerajswarnkar <neeraj.swarnkar51@gmail.com>
2024-11-10 14:42:33 +0530

    Added dashboard and workflows

--- container/src/App.js
+++ container/src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import { LinearProgress } from "@material-ui/core";
+
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -11,9 +17,16 @@ const generateClassName = createGenerateClassName({
 export default () => {
   return (
     <BrowserRouter>
-    <StylesProvider generateClassName={generateClassName}>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <Suspense fallback={<LinearProgress />}>
+            <Switch>
+              <Route path="/dashboard" component={DashboardLazy}></Route>
+              <Route path="/" component={MarketingApp}></Route>
+            </Switch>
+          </Suspense>
+        </div>
       </StylesProvider>
     </BrowserRouter>
   );
--- dashboard/config/webpack.prod.js
+++ dashboard/config/webpack.prod.js
@@ -2,19 +2,19 @@ const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJSON = require("../package.json");
-const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/dashboard/latest",
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
-      remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
-        dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`,
+      name: "dashboard",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./DashboardApp": "./src/bootstrap",
       },
       shared: packageJSON.dependencies,
     }),
